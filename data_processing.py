c6f2e0ad8e64090629d9e46a2ef53effd22c6359
bill <billsun9@gmail.com>
2021-03-06 22:12:29 -0500

    more data

--- data_processing.py
+++ data_processing.py
@@ -51,3 +51,45 @@ slice['AverageTemperature'] = slice['AverageTemperature'].round(3)
 slice_transpose = slice.pivot(index='State',columns='dt', values='AverageTemperature')
 # %%
 slice_transpose.to_csv('data/FormattedUsaTemp.csv')
+# %%
+import pandas as pd
+import matplotlib.pyplot as plt
+data = pd.read_csv('data/FormattedUsaTemp.csv')
+# %%
+data['mean'] = data.mean(axis=1)
+# %%
+new_data = data['State']
+# %%
+curYear = 1901
+tmp = pd.DataFrame()
+for i in range(1, len(data.columns)):
+    if data.columns[i][:4] == str(curYear):
+        if tmp.empty:
+            tmp = data[data.columns[i]]
+        else: # tmp is not none
+            tmp[data.columns[i]] = data[data.columns[i]]
+    else:
+        print(curYear)
+        new_data[str(curYear)] = tmp.mean(axis=1)
+        curYear = curYear + 1
+        
+
+new_data[str(curYear)] = tmp.mean(axis=0)
+
+# %%
+slice['year'] = slice['dt'].str[:4]
+# %%
+new_slice = slice.groupby(['State','year'])['AverageTemperature'].mean().reset_index()
+# %%
+new_slice['AverageTemperature'] = new_slice['AverageTemperature'].round(3)
+# %%
+new_slice_transpose = new_slice.pivot(index='State',columns='year', values='AverageTemperature')
+# %%
+new_slice_transpose.to_csv('data/FormattedUsaTempByYear.csv')
+
+
+
+
+
+
+
